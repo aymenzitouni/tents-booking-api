ce99d79d70380d12124bc7f8fc8f28c28db2f2be
aymenzitouni <a.zitouni@esi-sba.dz>
2024-01-24 13:34:35 +0100

    add swagger docs config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Tents Bookings System APIs Documentation')
+    .setDescription('Tents Bookings System APIs Documentation')
+    .setVersion('1.0')
+    .addTag('tents-booking')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
